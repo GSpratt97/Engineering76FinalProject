a22ed772558f1c8803ec9eb7902a3653e74d68ff
NJohnston94 <nathanj.94@hotmail.com>
2021-02-09 17:33:22 +0000

    Class can be used to test 'expand' button and that reports are displayed in chronological order so far with accompanying tests all passed

--- src/test/java/com/sparta/greg/pom/pagesTest/ReportTraineeTest.java
+++ src/test/java/com/sparta/greg/pom/pagesTest/ReportTraineeTest.java
@@ -2,10 +2,7 @@ package com.sparta.greg.pom.pagesTest;
 
 import com.sparta.greg.pom.pages.Login;
 import com.sparta.greg.pom.pages.ReportTrainee;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -22,8 +19,8 @@ public class ReportTraineeTest {
     private static          String          traineeUsername;
     private static          String          traineePassword;
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
         webDriver = new ChromeDriver();
         login     = new Login(webDriver);
 
@@ -39,12 +36,41 @@ public class ReportTraineeTest {
         login.enterPassword(traineePassword);
         login.clickSubmitButton();
 
-        reportTrainee   = new ReportTrainee(webDriver);
+        reportTrainee = new ReportTrainee(webDriver);
+        reportTrainee.setWeekReports();
+    }
+
+    @AfterEach
+    void closeWindow() {
+        webDriver.close();
     }
 
     @Test
     @DisplayName("Does http://localhost:8080/trainee/report load to WebDriver on class instantiation?")
     void isReportTraineePageLoaded() {
         Assertions.assertEquals("http://localhost:8080/trainee/report", reportTrainee.getUrl());
+    }
+
+    @Test
+    @DisplayName("Does the 'expand' button expand a weekly report?")
+    void doesReportExpand() {
+        Assertions.assertTrue(reportTrainee.doesExpandButtonExpand(reportTrainee.getWeekReports().get(0)));
+    }
+
+    @Test
+    @DisplayName("Does the 'expand' button collapse a weekly report?")
+    void doesReportCollapse() {
+        Assertions.assertTrue(reportTrainee.doesExpandButtonCollapse(reportTrainee.getWeekReports().get(0)));
+    }
+
+    @Test
+    @DisplayName("Do week numbers descend chronologically?")
+    void isWeekNumberCorrect() {
+        Assertions.assertTrue(reportTrainee.isWeekNumberCorrect());
+    }
+
+    @AfterAll
+    static void tearDown() {
+        webDriver.quit();
     }
 }
