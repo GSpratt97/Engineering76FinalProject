e01d277bbfc3543c55c259d05481ca0ac0196573
Jamie Hammond <33955664+jamiehammond@users.noreply.github.com>
2021-02-09 15:54:50 +0000

    Added methods for sidebar button and links.

--- src/test/java/com/sparta/greg/pom/pages/TraineePage.java
+++ src/test/java/com/sparta/greg/pom/pages/TraineePage.java
@@ -1,6 +1,30 @@
 package com.sparta.greg.pom.pages;
 
+import org.openqa.selenium.By;
+
 public abstract class TraineePage extends Page {
 
-    @FindBy(css = )
+    By traineeOptionsButton = new By.ByCssSelector("a[data-target*='#collapseUtilities']");
+    By feedbackFormLink = new By.ByCssSelector("a[href='/traineeRecentReport']");
+    By reportsHistoryLink = new By.ByCssSelector("a[href='/trainee/report']");
+    By attendanceHistoryLink = new By.ByCssSelector("a[href='/trainee/trainee-attendance']");
+
+    public void clickTraineeOptions() {
+        webDriver.findElement(traineeOptionsButton).click();
+    }
+
+    public FeedbackForm goToFeedbackForm() {
+        webDriver.findElement(feedbackFormLink).click();
+        return new FeedbackForm(webDriver);
+    }
+
+    public ReportsHistory goToReportsHistory() {
+        webDriver.findElement(reportsHistoryLink).click();
+        return new ReportsHistory(webDriver);
+    }
+
+    public AttendanceHistory goToAttendanceHistory() {
+        webDriver.findElement(attendanceHistoryLink).click();
+        return new AttendanceHistory(webDriver);
+    }
 }
