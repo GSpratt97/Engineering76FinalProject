e7fa159329df8142828011a54705cb19e7b7c496
Jamie Hammond <33955664+jamiehammond@users.noreply.github.com>
2021-02-09 15:07:43 +0000

    Bug fixes with clickWeekRow() in WeeklyAttendance.java, added test in WeeklyAttendanceTest.java.

--- src/test/java/com/sparta/greg/pom/pagesTest/WeeklyAttendanceTest.java
+++ src/test/java/com/sparta/greg/pom/pagesTest/WeeklyAttendanceTest.java
@@ -1,15 +1,47 @@
 package com.sparta.greg.pom.pagesTest;
 
+import com.sparta.greg.pom.pages.Login;
+import com.sparta.greg.pom.pages.WeeklyAttendance;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class WeeklyAttendanceTest {
 
+    private static final Properties properties = new Properties();
+    private static Login login;
+    private static WebDriver webDriver;
+    private static String trainerUsername;
+    private static String trainerPassword;
+
+    @BeforeAll
+    static void setup() {
+        webDriver = new ChromeDriver();
+        login = new Login(webDriver);
+        try {
+            properties.load(new FileReader("src/test/resources/login.properties"));
+            trainerUsername = properties.getProperty("trainerUsername");
+            trainerPassword = properties.getProperty("trainerPassword");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     @DisplayName("clickWeekRow returns true for populated list")
     void clickWeekRowReturnsTrueForPopulatedList() {
-        org.junit.jupiter.api.Assertions.fail("Not implemented");
+        login.logInAsTrainer(trainerUsername, trainerPassword);
+        WeeklyAttendance weeklyAttendance = new WeeklyAttendance(webDriver);
+        webDriver.get("http://localhost:8080/trainer/weekly-attendance");
+        Assertions.assertTrue(weeklyAttendance.clickWeekRow(1));
     }
 }
