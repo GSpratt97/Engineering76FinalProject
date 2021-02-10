ebfc7cc5fccd94bec81232f82a4c2d0bb9c445e2
JWeeden95 <72565776+JWeeden95@users.noreply.github.com>
2021-02-10 15:54:27 +0000

    Refactored to include sidebar changes

--- src/test/java/com/sparta/greg/pom/pages/TraineeAttendance.java
+++ src/test/java/com/sparta/greg/pom/pages/TraineeAttendance.java
@@ -1,5 +1,7 @@
 package com.sparta.greg.pom.pages;
 
+import com.sparta.greg.pom.pages.components.SideBarTrainee;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,18 +10,34 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class TraineeAttendance extends TraineePage{
+public class TraineeAttendance extends Page{
 
     @FindBy(id = "accordion1")
     List<WebElement> weeks;
 
+    SideBarTrainee sideBarTrainee;
+
+    public SideBarTrainee getSideBarTrainee(){
+        return sideBarTrainee;
+    }
+
     public TraineeAttendance(WebDriver webDriver){
-        this.webDriver = webDriver;
+        super(webDriver);
         PageFactory.initElements(webDriver, this);
+        sideBarTrainee = new SideBarTrainee(webDriver);
     }
 
     public void clickWeek(int week){
-        weeks.get(weeks.size() - week).click();
+        if (week > 0 && week <= weeks.size()) {
+            weeks.get(weeks.size() - week).click();
+        }
     }
 
+    public boolean isToggledOnWeek(int week){
+        String isToggled = webDriver.findElements(By.tagName("tbody")).get(weeks.size() - week).findElements(By.tagName("tr")).get(0).getAttribute("aria-expanded");
+        if (isToggled.equals("true")){
+            return true;
+        }
+        return false;
+    }
 }
