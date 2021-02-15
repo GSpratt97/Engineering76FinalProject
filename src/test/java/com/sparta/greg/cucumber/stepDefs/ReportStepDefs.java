1277453baf67152847cca97aacb50953bfc25373
NJohnston94 <nathanj.94@hotmail.com>
2021-02-15 10:35:26 +0000

    scenarios written in Report.feature and stepdefs declared in ReportStepDefs.java

--- src/test/java/com/sparta/greg/cucumber/stepDefs/ReportStepDefs.java
+++ src/test/java/com/sparta/greg/cucumber/stepDefs/ReportStepDefs.java
@@ -10,6 +10,7 @@ public class ReportStepDefs {
     //----------Given----------//
     @Given("I am logged in as a Trainee and I am on the Report page")
     public void iAmLoggedInAsATraineeAndIAmOnTheReportPage() {
+        
     }
 
     @Given("I am logged in as a Trainer and I am on Bill Birds Report Page")
@@ -33,6 +34,14 @@ public class ReportStepDefs {
     public void iClickTraineeOptionsOnTheTraineeSidebar() {
     }
 
+    @When("I click View on the Trainer Sidebar")
+    public void iClickViewOnTheTrainerSidebar() {
+    }
+
+    @When("I click Trainer Options on the Trainer Sidebar")
+    public void iClickTrainerOptionsOnTheTrainerSidebar() {
+    }
+
     //----------And----------//
     @And("I click Consultancy Skills")
     public void iClickConsultancySkills() {
@@ -54,6 +63,30 @@ public class ReportStepDefs {
     public void iClickAttendanceHistory() {
     }
 
+    @And("I click Class Management")
+    public void iClickClassManagement() {
+    }
+
+    @And("I click Trainee Management")
+    public void iClickTraineeManagement() {
+    }
+
+    @And("I click Add Weeks")
+    public void iClickAddWeeks() {
+    }
+
+    @And("I click Assessments")
+    public void iClickAssessments() {
+    }
+
+    @And("I click Enter Attendance")
+    public void iClickEnterAttendance() {
+    }
+
+    @And("I click Weekly Attendance")
+    public void iClickWeeklyAttendance() {
+    }
+
     //----------Then----------//
     @Then("the selected weekly report is expanded")
     public void theSelectedWeeklyReportIsExpanded() {
@@ -71,23 +104,55 @@ public class ReportStepDefs {
     public void iHaveReturnedToTraineeProfile() {
     }
 
-    @Then("I am taken to the Consultancy Skills page from the Report page")
-    public void iAmTakenToTheConsultancySkillsPageFromTheReportPage() {
+    @Then("I am taken to the Trainee Consultancy Skills page")
+    public void iAmTakenToTheTraineeConsultancySkillsPage() {
     }
 
-    @Then("I am taken to the Trainee Guide Page from the Report page")
-    public void iAmTakenToTheTraineeGuidePageFromTheReportPage() {
+    @Then("I am taken to the Trainee Guide page")
+    public void iAmTakenToTheTraineeGuidePage() {
     }
 
-    @Then("I am taken to the Feedback Form page from the Report Page")
-    public void iAmTakenToTheFeedbackFormPageFromTheReportPage() {
+    @Then("I am taken to the Trainee Feedback Form page")
+    public void iAmTakenToTheTraineeFeedbackFormPage() {
     }
 
-    @Then("I am taken to the Reports History Page from the Reports page")
-    public void iAmTakenToTheReportsHistoryPageFromTheReportsPage() {
+    @Then("I am taken to the Trainee Reports History page")
+    public void iAmTakenToTheTraineeReportsHistoryPage() {
     }
 
-    @Then("I am taken to the Attendance History page from the Report page")
-    public void iAmTakenToTheAttendanceHistoryPageFromTheReportPage() {
+    @Then("I am taken to the Trainee Attendance History page")
+    public void iAmTakenToTheTraineeAttendanceHistoryPage() {
+    }
+
+    @Then("I am taken to the Trainer Consultancy Skills page")
+    public void iAmTakenToTheTrainerConsultancySkillsPage() {
+    }
+
+    @Then("I am taken to the Trainer Trainee Guide page")
+    public void iAmTakenToTheTrainerTraineeGuidePage() {
+    }
+
+    @Then("I am taken to the Class Management page")
+    public void iAmTakenToTheClassManagementPage() {
+    }
+
+    @Then("I am taken to the Trainee Management page")
+    public void iAmTakenToTheTraineeManagementPage() {
+    }
+
+    @Then("I am taken to the Add Weeks page")
+    public void iAmTakenToTheAddWeeksPage() {
+    }
+
+    @Then("I am taken to the Assessments page")
+    public void iAmTakenToTheAssessmentsPage() {
+    }
+
+    @Then("I am taken to the Enter Attendance page")
+    public void iAmTakenToTheEnterAttendancePage() {
+    }
+
+    @Then("I am taken to the Weekly Attendance page")
+    public void iAmTakenToTheWeeklyAttendancePage() {
     }
 }
--- src/test/resources/features/Report.feature
+++ src/test/resources/features/Report.feature
@@ -39,52 +39,76 @@
     Given I am logged in as a Trainee and I am on the Report page
     When I click View on the Trainee Sidebar
     And I click Consultancy Skills
-    Then I am taken to the Consultancy Skills page from the Report page
+    Then I am taken to the Trainee Consultancy Skills page
 
   Scenario: Navigate to Trainee Guide from Report as Trainee
     Given I am logged in as a Trainee and I am on the Report page
     When I click View on the Trainee Sidebar
     And I click Trainee Guide
-    Then I am taken to the Trainee Guide Page from the Report page
+    Then I am taken to the Trainee Guide page
 
   Scenario: Navigate to Feedback Form from Report as Trainee
     Given I am logged in as a Trainee and I am on the Report page
     When I click Trainee Options on the Trainee Sidebar
     And I click Feedback Form
-    Then I am taken to the Feedback Form page from the Report Page
+    Then I am taken to the Trainee Feedback Form page
 
   Scenario: Navigate to Reports History from Report as Trainee
     Given I am logged in as a Trainee and I am on the Report page
     When I click Trainee Options on the Trainee Sidebar
     And I click Reports History
-    Then I am taken to the Reports History Page from the Reports page
+    Then I am taken to the Trainee Reports History page
 
   Scenario: Navigate to Attendance History from Report as Trainee
     Given I am logged in as a Trainee and I am on the Report page
     When I click Trainee Options on the Trainee Sidebar
     And I click Attendance History
-    Then I am taken to the Attendance History page from the Report page
+    Then I am taken to the Trainee Attendance History page
 
   Scenario: Navigate to Consultancy Skills from Report Details as Trainer
     Given I am logged in as a Trainer and I am on Bill Birds Report Page
+    When I click View on the Trainer Sidebar
+    And I click Consultancy Skills
+    Then I am taken to the Trainer Consultancy Skills page
 
   Scenario: Navigate to Trainee Guide from Report as Trainer
     Given I am logged in as a Trainer and I am on Bill Birds Report Page
+    When I click View on the Trainer Sidebar
+    And I click Trainee Guide
+    Then I am taken to the Trainer Trainee Guide page
 
   Scenario: Navigate to Class Management from Report as Trainer
     Given I am logged in as a Trainer and I am on Bill Birds Report Page
+    When I click Trainer Options on the Trainer Sidebar
+    And I click Class Management
+    Then I am taken to the Class Management page
 
   Scenario: Navigate to Trainee Management from Report as Trainer
     Given I am logged in as a Trainer and I am on Bill Birds Report Page
+    When I click Trainer Options on the Trainer Sidebar
+    And I click Trainee Management
+    Then I am taken to the Trainee Management page
 
   Scenario: Navigate to Add Weeks from Report as Trainer
     Given I am logged in as a Trainer and I am on Bill Birds Report Page
+    When I click Trainer Options on the Trainer Sidebar
+    And I click Add Weeks
+    Then I am taken to the Add Weeks page
 
   Scenario: Navigate to Assessments from Report as Trainer
     Given I am logged in as a Trainer and I am on Bill Birds Report Page
+    When I click Trainer Options on the Trainer Sidebar
+    And I click Assessments
+    Then I am taken to the Assessments page
 
   Scenario: Navigate to Enter Attendance from Report as Trainer
     Given I am logged in as a Trainer and I am on Bill Birds Report Page
+    When I click Trainer Options on the Trainer Sidebar
+    And I click Enter Attendance
+    Then I am taken to the Enter Attendance page
 
   Scenario: Navigate to Weekly Attendance from Report as Trainer
     Given I am logged in as a Trainer and I am on Bill Birds Report Page
+    When I click Trainer Options on the Trainer Sidebar
+    And I click Weekly Attendance
+    Then I am taken to the Weekly Attendance page
