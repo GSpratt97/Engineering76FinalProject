a7a86d058d4dbd6edf3f00806c0db0b40c250ea0
Asakar <asakarh@gmail.com>
2021-02-15 15:48:12 +0000

    Tests for demo

---
+++ src/test/java/com/sparta/greg/demo/TestBed.java
@@ -0,0 +1,23 @@
+package com.sparta.greg.demo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestBed {
+
+    // Create a trainee
+    // Assign to a existing class
+
+    @Test
+    @DisplayName("longTest")
+    void longTest() {
+
+    }
+
+    @Test
+    @DisplayName("withPOMTest")
+    void withPomTest() {
+
+    }
+
+}
