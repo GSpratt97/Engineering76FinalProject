dc3db4761457b6529b50634a884cc579b0884b80
Andrei Pavel <samurahh@gmail.com>
2021-02-16 22:04:13 +0000

    Bug fix attempt. Text can be introduced in text boxes.

--- src/test/java/com/sparta/greg/cucumber/stepDefs/ManageTraineesStepdefs.java
+++ src/test/java/com/sparta/greg/cucumber/stepDefs/ManageTraineesStepdefs.java
@@ -2,6 +2,7 @@ package com.sparta.greg.cucumber.stepdefs;
 
 import com.sparta.greg.pom.pages.components.Login;
 import com.sparta.greg.pom.pages.trainer.ManageTrainee;
+import io.cucumber.java.After;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -9,6 +10,9 @@ import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class ManageTraineesStepdefs {
 
@@ -22,17 +26,20 @@ public class ManageTraineesStepdefs {
     @Given("I am logged in as a Trainer and on the Manage Trainee Page.")
     public void iAmLoggedInAsATrainerAndOnTheManageTraineePage() {
         webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.get("http://localhost:8080");
         login = new Login(webDriver);
         manageTrainee = login
                 .logInAsTrainer("MGadhvi@sparta.com", "startrek")
                 .getSideBarTrainer()
                 .clickTrainerOptions()
                 .goToTraineeManagement();
+        createTrainee = manageTrainee.createTrainee();
+        deleteTrainee = manageTrainee.deleteTrainee();
     }
 
     @Given("I filled out the Create Trainee form with name {string} {string}, email {string} in {string}.")
     public void iFilledOutTheCreateTraineeFormWithNameFirstNameLastNameEmailEmailInClass(String var0, String var1, String var2, String var3) {
-        createTrainee = manageTrainee.createTrainee();
         createTrainee.withFirstName(var0)
                 .withLastName(var1)
                 .withEmail(var2)
@@ -42,7 +49,7 @@ public class ManageTraineesStepdefs {
 
     @When("I click on Create New Trainee on Manage Trainee Page.")
     public void iClickOnCreateNewTraineeOnManageTraineePage() {
-        createTrainee.perform();
+        createTrainee = createTrainee.perform();
     }
 
     @Then("I am getting a validation warning for invalid data in Create Trainee form.")
@@ -53,7 +60,7 @@ public class ManageTraineesStepdefs {
                 isWarning = true;
             }
         }
-        if(!isWarning){
+        if (!isWarning) {
             cleanupDelete(name);
         }
         Assertions.assertTrue(isWarning);
@@ -61,10 +68,11 @@ public class ManageTraineesStepdefs {
 
     @Then("I am getting {string} message in Create Trainee form.")
     public void iAmGettingMessageInCreateTraineeForm(String arg0) {
-        if(createTrainee.getMessage().contains(arg0)){
+        String message = createTrainee.getMessage();
+        if (createTrainee.getMessage().contains(arg0)) {
             cleanupDelete(name);
         }
-        Assertions.assertTrue(createTrainee.getMessage().contains(arg0));
+        Assertions.assertTrue(message.contains(arg0));
     }
 
     @Given("I have a trainee registered with {string}.")
@@ -72,8 +80,7 @@ public class ManageTraineesStepdefs {
         iFilledOutTheCreateTraineeFormWithNameFirstNameLastNameEmailEmailInClass("FirstNameExample", "LastNameExample", arg0, "Engineering 76");
         name = "FirstNameExample LastNameExample";
         iClickOnCreateNewTraineeOnManageTraineePage();
-        manageTrainee.getSidebar().clickTrainerOptions().goToTraineeManagement();
-
+        iAmLoggedInAsATrainerAndOnTheManageTraineePage();
     }
 
     @Given("I am not selecting any trainee in Delete Trainee form.")
@@ -102,14 +109,17 @@ public class ManageTraineesStepdefs {
         deleteTrainee.selectTrainee(arg0);
     }
 
-    @Given("I entered name {string}.")
-    public void iEnteredName(String arg0) {
-        createTrainee = manageTrainee.createTrainee();
+    @After("ManageTrainee")
+    void quit() {
+        webDriver.quit();
     }
 
-    void cleanupDelete(String name){
-        manageTrainee = manageTrainee.getSidebar().clickTrainerOptions().goToTraineeManagement();
-        deleteTrainee = manageTrainee.deleteTrainee();
+    void cleanupDelete(String name) {
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
+        webDriver = new ChromeDriver(option);
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        iAmLoggedInAsATrainerAndOnTheManageTraineePage();
         deleteTrainee.selectTrainee(name).perform();
     }
 }
