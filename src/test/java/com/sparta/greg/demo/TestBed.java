828b39a4e4e3b9653b350f78c3f8be30c06d9499
Asakar <asakarh@gmail.com>
2021-02-16 13:35:06 +0000

    Tests for demo

--- src/test/java/com/sparta/greg/demo/TestBed.java
+++ src/test/java/com/sparta/greg/demo/TestBed.java
@@ -1,7 +1,6 @@
 package com.sparta.greg.demo;
 
 import com.sparta.greg.pom.pages.components.Login;
-
 import com.sparta.greg.pom.pages.components.PropertyLoader;
 import com.sparta.greg.pom.pages.trainer.ClassManagement;
 import com.sparta.greg.pom.pages.trainer.HomeTrainer;
@@ -11,19 +10,10 @@ import com.sparta.greg.pom.webDriverFactory.WebDriverType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 
 public class TestBed {
@@ -34,11 +24,6 @@ public class TestBed {
     private ManageTrainee manageTrainee;
     private ClassManagement classManagement;
     private HomeTrainer homeTrainer;
-
-
-    // Create a trainee
-    // Assign to a existing class
-    // delete trainee after
 
     private static WebDriver webDriver;
 
@@ -67,23 +52,22 @@ public class TestBed {
         webDriver.findElement(By.cssSelector("input[id='lastName']")).sendKeys("long");
         Select dropdownClass = new Select(webDriver.findElement(By.cssSelector("select[id*='groupId']")));
         dropdownClass.selectByVisibleText("Engineering 65");
-//        webDriver.findElement(By.cssSelector("button[type='submit']")).click();
-//        // I want to use the pom this is too long
-//        // go to management class
-//        webDriver.findElement(By.cssSelector("a[data-target*='#collapsePages']")).click();
-//        webDriver.findElement(By.cssSelector("a[href*='/trainer/manageClass']")).click();
-//        // assign trainee to class
-//        Select dropdownTrainee = new Select(webDriver.findElement(By.cssSelector("select[name='traineeId']")));
-//        dropdownTrainee.selectByVisibleText("Nathan Long");
-//        Select dropdownTraineeClass = new Select(webDriver.findElement(By.cssSelector("select[name='groupId']")));
-//        dropdownTraineeClass.selectByVisibleText("Engineering 65");
-//        // Go back to trainee management
-//        webDriver.findElement(By.cssSelector("a[data-target*='#collapsePages']")).click();
-//        webDriver.findElement(By.cssSelector("a[href*='/trainer/manageTrainee']")).click();
-//        // delete trainee
-//        Select dropdownDeleteTrainee = new Select(webDriver.findElement(By.cssSelector("select[name*='trainee']")));
-//        dropdownDeleteTrainee.selectByVisibleText("Nathan Long");
-//        webDriver.findElement(By.cssSelector("form[action='/trainer/deleteTrainee'] button[type='submit']")).click();
+        webDriver.findElement(By.cssSelector("button[type='submit']")).click();
+        // go to management class
+        webDriver.findElement(By.cssSelector("a[data-target*='#collapsePages']")).click();
+        webDriver.findElement(By.cssSelector("a[href*='/trainer/manageClass']")).click();
+        // assign trainee to class
+        Select dropdownTrainee = new Select(webDriver.findElement(By.cssSelector("select[name='traineeId']")));
+        dropdownTrainee.selectByVisibleText("Nathan Long");
+        Select dropdownTraineeClass = new Select(webDriver.findElement(By.cssSelector("select[name='groupId']")));
+        dropdownTraineeClass.selectByVisibleText("Engineering 65");
+        // Go back to trainee management
+        webDriver.findElement(By.cssSelector("a[data-target*='#collapsePages']")).click();
+        webDriver.findElement(By.cssSelector("a[href*='/trainer/manageTrainee']")).click();
+        // delete trainee
+        Select dropdownDeleteTrainee = new Select(webDriver.findElement(By.cssSelector("select[name*='trainee']")));
+        dropdownDeleteTrainee.selectByVisibleText("Nathan Long");
+        webDriver.findElement(By.cssSelector("form[action='/trainer/deleteTrainee'] button[type='submit']")).click();
     }
 
     @Test
