3a864695b290dc888e923ab74125c6e0fe977834
MrFletcha <jwefletcher@hotmail.co.uk>
2021-02-16 16:59:45 +0000

    Merges from dev

--- src/test/java/com/sparta/greg/cucumber/TestRunner.java
+++ src/test/java/com/sparta/greg/cucumber/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions
         (
-                tags = "@AttendanceMultiple",
+                tags = "@Testing",
                 features = {"src/test/resources/features"},
                 plugin = {"pretty", "html:target/reports/htmlReport.html", "json:target/reports/jsonReport.json"}
         )
